0f94c8fa733e217cb3aab587c8c2d6485d37652d
Ivan Henrique Santos <ivan.henrique.s.1406@gmail.com>
2021-07-13 23:25:53 -0300

    First

---
+++ digital.innovation.one.core/src/digital/innovation/one/core/Runner.java
@@ -0,0 +1,16 @@
+package digital.innovation.one.core;
+
+
+import digital.innovation.one.utils.Calculadora;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Calculadora cal = new Calculadora();
+        System.out.println(cal.div(10, 5));
+
+    }
+
+
+}
